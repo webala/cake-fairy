0f4dd8490d2c26b026a8bdd43f7e18b7223cf55d
webala <webdspam@gmail.com>
2022-08-12 11:56:59 +0300

    testify section styling updated

--- components/Testify.js
+++ components/Testify.js
@@ -31,31 +31,34 @@ function Testify() {
     setComment("");
   };
   return (
-    <div className="py-10">
+    <div className="py-10 w-full">
       <form onSubmit={submitComment}>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-start">
           <label className="text-xl mx-5 mt-5 heading">
             Share your experience with Cake Fairy
           </label>
-          <div className="flex flex-col sm:w-1/2 items-center pt-10">
-            <label className="text-xl mx-5 mt-5 top-0 left-10">Your Name</label>
+          <div className="flex flex-col sm:w-full items-start pt-10">
+            <label className="text-xl mt-5">Your Name</label>
             <input
-              className="rounded h-10 m-5 w-1/2 text-black"
+              className="rounded h-10 w-1/2 text-black"
               type="text"
               onChange={(e) => setName(e.target.value)}
               required
               value={name}
             />
+            <label className="text-xl mt-5">What did you like about our service?</label>
             <textarea
-              className="rounded h-20 sm:h-40 m-5 w-full text-black"
+              className="rounded h-20 sm:h-40 my-5 w-2/3 md:w-1/2 text-black"
               type="text"
               onChange={(e) => setComment(e.target.value)}
               required
               value={comment}
             />
-            <StarRating rating={rating} setRating={setRating} />
+            <div className="relative left-28">
+              <StarRating rating={rating} setRating={setRating} />
+            </div>
             <button
-              className="rounded bg-orange-900 sm:w-1/2 p-2 hover:scale-110 transition duration-300"
+              className="rounded bg-orange-900 w-1/2  md:w-44 p-2 hover:scale-110 transition duration-300"
               type="submit"
             >
               Leave Comment
--- components/Testimonials.js
+++ components/Testimonials.js
@@ -3,14 +3,16 @@ import TestimonyItem from "./TestimonyItem"
 
 function Testimonials({clientStories}) {
   return (
-    <div className="testimonials px-4 md:px-20 xl:px-44">
+    <div className="testimonials px-4 md:px-20 xl:px-44 flex flex-col items-center">
+      <div className="w-full">
         <h1 className="text-xl heading py-7">Client stories</h1>
         <div className="flex flex-wrap justify-start items-center">
             {clientStories.map((story, index) => {
               return <TestimonyItem name={story.name} testimony={story.comment} rating={story.rating} key={index}/>
             })}
         </div>
-        <Testify clientStories={clientStories} />
+      </div>
+      <Testify clientStories={clientStories} />
     </div>
 
   )
