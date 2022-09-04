b7d59b33ff93156787ceb014f7a49f2cc828dd2f
webala <webdspam@gmail.com>
2022-09-04 14:03:06 +0300

    UPDATE: next auth version

--- daraja/index.js
+++ daraja/index.js
@@ -18,7 +18,7 @@ const inititateStkPush = async (phone, amount) => {
     PartyB: "174379",
     PhoneNumber: phone,
     CallBackURL:
-      "https://8b4d-105-163-28-112.eu.ngrok.io/api/daraja/mpesa-callback",
+      "https://e6cc-105-163-61-89.in.ngrok.io/api/daraja/mpesa-callback",
     AccountReference: "Cake Fairy",
     TransactionDesc: "Make Payment for cake",
   };
