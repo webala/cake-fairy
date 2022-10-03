25eabe96a5fd9dd109e2892a5519b6300bcdf3a0
webala <webdspam@gmail.com>
2022-10-03 18:57:59 +0300

    REFACTOR: menu

--- components/Menu.js
+++ components/Menu.js
@@ -2,22 +2,12 @@ import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { AiOutlineArrowRight, AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useState } from "react";
+
 function Menu({ categories, pathname, setFlavoursSelected, flavorsSelected }) {
   const [isLoading, setIsLoading] = useState(false);
   return (
-    <div className="menu px-4 md:px-20 xl:px-44" id="menu">
-      {pathname !== "/order" && (
-        <h1 className="text-2xl py-10 heading">Menu</h1>
-      )}
-      {pathname === "/order" && (
-        <div>
-          <h1 className="text-2xl py-10 heading">Place Order</h1>
-          <p className="text-2xl text-white">
-            Click on the flavour of you choice.
-          </p>
-        </div>
-      )}
-      <div className="border-yellow-300">
+    <div className="menu px-4 md:px-20 xl:px-64" id="menu">
+      <div className="">
         {categories.map((item, index) => {
           return (
             <MenuItem
@@ -30,28 +20,27 @@ function Menu({ categories, pathname, setFlavoursSelected, flavorsSelected }) {
           );
         })}
       </div>
-      {pathname !== "/order" && (
-        <div>
-          <Link href="/order">
-            <button
-              onClick={() => setIsLoading(true)}
-              className="order-btn group rounded bg-orange-900 sm:w-1/2 p-2 hover:scale-110 transition duration-300 text-xl m-10 flex justify-evenly items-center"
-            >
-              <p>Place Order</p>
-              <AiOutlineArrowRight
-                className={
-                  isLoading
-                    ? "invisible"
-                    : "order-btn-icon invisible group-hover:visible duration-500"
-                }
-              />
-              <AiOutlineLoading3Quarters
-                className={isLoading ? "visible animate-spin" : "invisible"}
-              />
-            </button>
-          </Link>
-        </div>
-      )}
+
+      <div>
+        <Link href="/order">
+          <button
+            onClick={() => setIsLoading(true)}
+            className="order-btn group rounded bg-backgroundSecondary p-2 hover:scale-110 transition duration-300 text-xl m-10 flex justify-evenly items-center"
+          >
+            <p>Place Order</p>
+            <AiOutlineArrowRight
+              className={
+                isLoading
+                  ? "invisible"
+                  : "order-btn-icon invisible group-hover:visible duration-500"
+              }
+            />
+            <AiOutlineLoading3Quarters
+              className={isLoading ? "visible animate-spin" : "invisible"}
+            />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
